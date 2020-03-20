080a6554f9afebc75c306cbf8d82f0431a0b6460
emontealvo <edwin.montealvo@gmail.com>
2020-03-20 00:13:32 -0600

    defined Being Class properties and methods

--- src/being.js
+++ src/being.js
@@ -1,4 +1,17 @@
 class Being {
-}
+	constructor(name, species) {
+		this.name = name
+		this.isAlive = true
+		this.species = species
+		this.credits = 0 
+		this.updateCredits = function(creditValue) {
+			return (this.credits += creditValue)
+		}
+	}
+
+	}
+
+
+
 
 module.exports = Being;
